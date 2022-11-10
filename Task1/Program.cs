f6114dec060f8a8a8f1dec50d29a77e47cc6361d
Novikovsyi Oleksii <sana22_nov@student.ztu.edu.ua>
2022-11-10 17:44:21 +0200

    Change 3 messages for arbitrary messages

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+﻿Console.WriteLine("Arbitrary message");
+Console.WriteLine("Arbitrary message");
+Console.WriteLine("Arbitrary message");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
